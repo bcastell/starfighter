5b3da579129289478484e2e5c0e4a8bcdcab0a8d
Brandon Castellanos <bcastellanos87@gmail.com>
2017-07-06 19:16:21 -0700

    player moves up and down

--- app/js/controls.js
+++ app/js/controls.js
@@ -4,10 +4,16 @@
 	var controls = window.starfighter.controls = {
 		moveLeft   : false,
 		moveRight  : false,
+		moveUp     : false,
+		moveDown   : false,
 		A          : 65,
 		D		   : 68,
+		W		   : 87,
+		S		   : 83,
 		leftArrow  : 37,
-		rightArrow : 39
+		rightArrow : 39,
+		upArrow	   : 38,
+		downArrow  : 40
 	};
 
 	function keydownHandler(event) {
@@ -16,6 +22,12 @@
 
 		if (event.keyCode == controls.D || event.keyCode == controls.rightArrow)
 			controls.moveRight = true;
+
+		if (event.keyCode == controls.W || event.keyCode == controls.upArrow)
+			controls.moveUp = true;
+
+		if (event.keyCode == controls.S || event.keyCode == controls.downArrow)
+			controls.moveDown = true;
 	}
 
 	function keyupHandler(event) {
@@ -24,6 +36,12 @@
 
 		if (event.keyCode == controls.D || event.keyCode == controls.rightArrow)
 			controls.moveRight = false;
+
+		if (event.keyCode == controls.W || event.keyCode == controls.upArrow)
+			controls.moveUp = false;
+
+		if (event.keyCode == controls.S || event.keyCode == controls.downArrow)
+			controls.moveDown = false;
 	}
 
 	document.addEventListener("keydown", keydownHandler);
--- app/js/player.js
+++ app/js/player.js
@@ -16,6 +16,7 @@
 		this.renderY = context.canvas.height - this.renderHeight;
 
 		this.dx = 6;
+		this.dy = 6;
 	};
 
 	Player.prototype.render = function() {
@@ -34,6 +35,15 @@
 		var canMoveRight = this.renderX + this.renderWidth <= this.context.canvas.width - this.dx;
 		if (controls.moveRight && canMoveRight)
 			this.renderX += this.dx
+
+		var canMoveUp = this.renderY >= this.dy;
+		if (controls.moveUp && canMoveUp)
+			this.renderY -= this.dy;
+
+		var canMoveDown = this.renderY + this.renderHeight <= this.context.canvas.height - this.dy;
+		if (controls.moveDown && canMoveDown)
+			this.renderY += this.dy;
+
 	};
 
 })();
